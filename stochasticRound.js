36ac8a20e474f2ef51e6930f49605d93c8891e26
root <jtas1999@gmail.com>
2017-10-30 16:43:15 +0000

    You can now use it in the command line :)

--- stochasticRound.js
+++
@@ -1,9 +0,0 @@
-module.exports.stochasticRound = function(n) {
-    var truncN = Math.trunc(n)
-    if (n === null || isNaN(n)) { return NaN; }
-    var p1 = n - truncN
-    var p2 = n - p1
-    var ran = Math.random(); // gen random number between 0 and 1
-    if (ran < p1) return truncN;
-    else if (ran < p2) return truncN + 1;
-}
